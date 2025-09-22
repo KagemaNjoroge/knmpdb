4eb786c5dcd2ebd27eba3dd8a1d56db80605c801
kagema <reecejames934@gmail.com>
2025-09-22 11:23:03 +0300

    Add logging configuration to settings.py

--- knmpdb/settings.py
+++ knmpdb/settings.py
@@ -162,3 +162,23 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
